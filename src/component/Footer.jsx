a7b9f9d7615807d70dac4ef3d547bd9d4b485e4c
HONEYCOMB\Varsha <varsha.honeycomb@gmail.com>
2023-03-24 18:27:28 +0530

    added components

---
+++ src/component/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Footer
+        <section className='container '>
+            <div className='grid grid-cols-4 gap-4'>
+                <div>Copyright © 2023 Varsha. All Rights Reserved.</div>
+                <div>Designed by Harnish Design</div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Footer
